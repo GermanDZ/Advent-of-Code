713d2c684df9055be9120fdc47130731e72bcca1
German Del Zotto <g.delzotto@stuart.com>
2021-12-08 17:14:41 +0100

    Day 5 - Part 2

---
+++ 2021/day-05/part2.rb
@@ -0,0 +1,36 @@
+input = File.read("input")
+
+map = []
+
+input.split("\n").map { |line| line.split(" -> ") }.map do |coord1, coord2|
+  x1, y1 = coord1.split(",").map(&:to_i)
+  x2, y2 = coord2.split(",").map(&:to_i)
+
+  if (x1 == x2 || y1 == y2)
+    src_x = [x1, x2].min
+    dest_x = [x1, x2].max
+    src_y = [y1, y2].min
+    dest_y = [y1, y2].max
+    (src_x..dest_x).each do |x|
+      (src_y..dest_y).each do |y|
+        row = map[y] ||= [] 
+        row[x] ||= 0
+        row[x] += 1
+      end
+    end
+  else    
+    from, to, dir_y, y0 = if x1 < x2
+                 [x1, x2, y2 > y1 ? 1 : -1, y1]
+               else
+                 [x2, x1, y2 > y1 ? -1 : 1, y2]
+               end
+    (from..to).each_with_index do |z, i|
+      row = map[y0 + i * dir_y] ||= [] 
+      row[z] ||= 0
+      row[z] += 1
+    end  
+  end
+end
+
+result = map.reduce(0) { |sum, row| (row || []).reduce(sum) { |sum, pos| pos.to_i >= 2 ? sum + 1 : sum } }
+puts "result: #{result}"
--- 2021/day-05/solution.md
+++ 2021/day-05/solution.md
@@ -3,4 +3,6 @@
 I'm way behind schedule, I will try to solve the problem spending just a few minutes.
 
 Answer Part 1: 6856
-Answer Part 2:
+Answer Part 2: 20666
+
+Solving the Part 2, I've discovered the Part 1 is working but is doing some not needed work.
